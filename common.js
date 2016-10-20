f6a288e16ff27cae3bf04d4a3bf8710c579a4385
cbattista <battista.christian@gmail.com>
2016-10-20 08:19:57 -0400

    adding ordinality task

--- common.js
+++ common.js
@@ -2,6 +2,30 @@
 //Stanford Cognitive and Systems Neuroscience Lab
 
 //UTILITY FUNCTIONS
+Array.prototype.equals = function (array) {
+    // if the other array is a falsy value, return
+    if (!array)
+        return false;
+
+    // compare lengths - can save a lot of time
+    if (this.length != array.length)
+        return false;
+
+    for (var i = 0, l=this.length; i < l; i++) {
+        // Check if we have nested arrays
+        if (this[i] instanceof Array && array[i] instanceof Array) {
+            // recurse into the nested arrays
+            if (!this[i].equals(array[i]))
+                return false;
+        }
+        else if (this[i] != array[i]) {
+            // Warning - two different object instances will never be equal: {x:20} != {x:20}
+            return false;
+        }
+    }
+    return true;
+}
+
 function makeSVG(circles){
 	output = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"350\" height=\"600\">";
 	for (i=0;i<circles.length;i++)  {
